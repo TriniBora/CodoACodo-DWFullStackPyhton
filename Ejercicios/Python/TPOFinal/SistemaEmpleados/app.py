10569c061bfa1272ea456afb6d7f1bbc6404baa3
Trinidad Boragini <mtboragini@gmail.com>
2021-07-08 12:26:33 -0300

    Mostrado de empleados en el documento HTML

--- Ejercicios/Python/TPOFinal/SistemaEmpleados/app.py
+++ Ejercicios/Python/TPOFinal/SistemaEmpleados/app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from flask import render_template, request
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app=Flask(__name__)  # aca creamos la aplicacion (objeto de Flask)
 
@@ -19,12 +20,15 @@ mysql.init_app(app)
 
 @app.route('/')     # cuando el usuario escriba / se va a buscar el archivo index.html
 def index():
-    sql="INSERT INTO `sistema`.`empleados` (`nombre`, `correo`,`foto`) VALUES ('juan', 'juan@gmail.com','foto.jpg');"
+    #sql="INSERT INTO `sistema`.`empleados` (`nombre`, `correo`,`foto`) VALUES ('juan', 'juan@gmail.com','foto.jpg');"
+    sql = "SELECT * FROM `sistema` . `empleados`;"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
+    empleados = cursor.fetchall()
+    #print(empleados)
     conn.commit()
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados=empleados)
 
 @app.route('/create')
 def create():
@@ -36,9 +40,16 @@ def storage():
     _correo=request.form['txtCorreo']
     _foto=request.files['txtFoto']
 
-    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`,`foto`) VALUES (NULL, %s, %s, %s);"
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
 
-    datos=(_nombre, _correo, _foto.filename)
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
+    sql = "INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`,`foto`) VALUES (NULL, %s, %s, %s);"
+
+    datos=(_nombre, _correo, nuevoNombreFoto)
 
     conn =mysql.connect()
     cursor = conn.cursor()
